367261c80393b5daa031ed9bb7b8270b2974c566
jeeeun <jek.1976534@gmail.com>
2025-07-18 19:11:50 +0900

    refactor: reimplement canvas editor with simplified state management and improved rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CanvasEditor } from './components/CanvasEditor.simple';
+import { CanvasEditor } from './components/CanvasEditor';
 import './App.css';
 
 function App() {
